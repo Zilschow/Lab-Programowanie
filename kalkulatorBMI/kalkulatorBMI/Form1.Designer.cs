2b40264cce25b20519d2c52f62e4e45229c29660
Zilschow <83867751+Zilschow@users.noreply.github.com>
2021-05-18 22:20:21 +0200

    Add files via upload

---
+++ kalkulatorBMI/kalkulatorBMI/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿
+namespace kalkulatorBMI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.radKob = new System.Windows.Forms.RadioButton();
+            this.radMen = new System.Windows.Forms.RadioButton();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Podaj wzrost w [cm]";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Wybierz płeć";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(161, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 2;
+            // 
+            // radKob
+            // 
+            this.radKob.AutoSize = true;
+            this.radKob.Location = new System.Drawing.Point(161, 64);
+            this.radKob.Name = "radKob";
+            this.radKob.Size = new System.Drawing.Size(65, 19);
+            this.radKob.TabIndex = 3;
+            this.radKob.TabStop = true;
+            this.radKob.Text = "Kobieta";
+            this.radKob.UseVisualStyleBackColor = true;
+            this.radKob.CheckedChanged += new System.EventHandler(this.radKob_CheckedChanged);
+            // 
+            // radMen
+            // 
+            this.radMen.AutoSize = true;
+            this.radMen.Location = new System.Drawing.Point(239, 66);
+            this.radMen.Name = "radMen";
+            this.radMen.Size = new System.Drawing.Size(82, 19);
+            this.radMen.TabIndex = 4;
+            this.radMen.TabStop = true;
+            this.radMen.Text = "Mężczyzna";
+            this.radMen.UseVisualStyleBackColor = true;
+            this.radMen.CheckedChanged += new System.EventHandler(this.radMen_CheckedChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(6, 22);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(95, 19);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Standardowa";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.groupBox1.Location = new System.Drawing.Point(341, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(147, 83);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Wybierz wagę";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(7, 49);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(64, 19);
+            this.checkBox2.TabIndex = 6;
+            this.checkBox2.Text = "Idealna";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(215, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 48);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Oblicz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(41, 181);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 32);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Wynik";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(527, 251);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.radMen);
+            this.Controls.Add(this.radKob);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton radKob;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radMen;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ kalkulatorBMI/kalkulatorBMI/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kalkulatorBMI
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radMen_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radKob_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            if (checkBox1.Checked & (radKob.Checked | radMen.Checked))
+            {
+                try
+                {
+                    float wz = float.Parse(textBox1.Text);
+
+                    label3.Text = (wz - 100).ToString();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Nie podano wzrostu", "Błąd", MessageBoxButtons.OK);
+                    throw;
+                }
+            }
+            else
+            {
+
+                if (checkBox2.Checked & radKob.Checked)
+                {
+                    try
+                    {
+                        float wz = float.Parse(textBox1.Text);
+
+                        label3.Text = ((wz - 100) * 0.85).ToString();
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("Nie podano wzrostu", "Błąd", MessageBoxButtons.OK);
+
+                        throw;
+
+                    }
+                }
+                else
+                {
+                    if (checkBox2.Checked & radMen.Checked)
+                    {
+
+                        try
+                        {
+                            float wz = float.Parse(textBox1.Text);
+
+                            label3.Text = ((wz - 100) * 0.9).ToString();
+                        }
+                        catch (Exception)
+                        {
+                            MessageBox.Show("Nie podano wzrostuMMM", "Błąd", MessageBoxButtons.OK);
+                        }
+
+                    }
+                }
+
+            }
+
+        }
+    }
+}
+
