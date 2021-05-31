8c0804d6acedd4ce548ad3e3c2ef71f58bbec8bf
Zilschow <83867751+Zilschow@users.noreply.github.com>
2021-05-31 20:20:41 +0200

    Add files via upload

---
+++ lab4_zad1/lab4_zad1/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab4_zad1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public class Student
+    {
+        public string imie { get; set; }
+        public string nazwisko { get; set; }
+        public int NrIndeksu { get; set; }
+        public string wydzial { get; set; }
+
+        public Student(string imie, string nazwisko, int NrIndeksu, string wydzial)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.NrIndeksu = NrIndeksu;
+            this.wydzial = wydzial;
+        }
+        public Student()
+            : this("","", 0, "")
+        { }
+    }
+    public partial class MainWindow : Window
+    {
+        public List<Student> ListaStudentow { get; set; }
+
+        public MainWindow()
+        {
+            ListaStudentow = new List<Student>()
+            {
+                new Student(){ imie = "Jan", nazwisko= "Kowalski", NrIndeksu=1234, wydzial= "KIS"},
+                new Student(){ imie = "Andrzej", nazwisko= "Nowak", NrIndeksu=2345, wydzial= "KIS"},
+                new Student(){ imie = "Ted", nazwisko= "Kaczynski", NrIndeksu=3456, wydzial= "KIS"},
+                new Student(){ imie = "Krzysztof", nazwisko= "Baranski", NrIndeksu=4567, wydzial= "KIS"},
+            };
+
+            InitializeComponent();
+
+            dgStudent.Columns.Add(new DataGridTextColumn() { Header = "Imie", Binding = new Binding("imie") });
+            dgStudent.Columns.Add(new DataGridTextColumn() { Header = "Nazwisko", Binding = new Binding("nazwisko") });
+            dgStudent.Columns.Add(new DataGridTextColumn() { Header = "Nr Indeksu", Binding = new Binding("NrIndeksu") });
+            dgStudent.Columns.Add(new DataGridTextColumn() { Header = "wydzial", Binding = new Binding("wydzial") });
+
+            dgStudent.AutoGenerateColumns = false;
+            dgStudent.ItemsSource = ListaStudentow;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new Window1();
+            if (dialog.ShowDialog() == true)
+            {
+                ListaStudentow.Add(dialog.student);
+                dgStudent.Items.Refresh();
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (dgStudent.SelectedItem is Student)
+                ListaStudentow.Remove((Student)dgStudent.SelectedItem);
+            dgStudent.Items.Refresh();
+        }
+    }
+}
---
+++ lab4_zad1/lab4_zad1/Window1.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
+namespace lab4_zad1
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+
+
+    public partial class Window1 : Window
+    {
+        public Student student;
+        public Window1(Student student = null)
+        {
+            InitializeComponent();
+            if (student != null)
+            {
+                tbImie.Text = student.imie;
+                tbNazwisko.Text = student.nazwisko;
+                tbNumer.Text = student.NrIndeksu.ToString();
+                tbWydzial.Text = student.wydzial;
+            }
+            this.student = student ?? new Student();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!Regex.IsMatch(tbImie.Text, @"^\p{Lu}\p{Ll}{1,12}$") ||
+                !Regex.IsMatch(tbNazwisko.Text, @"^\p{Lu}\p{Ll}{1,12}$") ||
+                !Regex.IsMatch(tbWydzial.Text, @"^\p{Lu}{1,12}$") ||
+                !Regex.IsMatch(tbNumer.Text, @"^[0-9]{4,10}$"))
+            {
+                MessageBox.Show("Podano bledne dane.");
+                return;
+            }
+            student.imie = tbImie.Text;
+            student.nazwisko = tbNazwisko.Text;
+            student.wydzial = tbWydzial.Text;
+            student.NrIndeksu = int.Parse(tbNumer.Text);
+
+            this.DialogResult = true;
+        }
+    }
+}
